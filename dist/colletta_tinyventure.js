05c22136acc31f7cc2771ee1fc0e5eccbf6bf87b
steliviere <32293736+steliviere@users.noreply.github.com>
2018-06-23 03:37:48 +0900

    Update colletta_tinyventure.js

--- dist/colletta_tinyventure.js
+++ dist/colletta_tinyventure.js
@@ -138,7 +138,7 @@ var game=function(d)
 				case 1: case 2: case 3: case 4: case 5:
 				case 6: case 7: case 8: case 9:
 				case 25: case 27:
-				case 10:a.setCollider(0,0,d.tileSize,d.tileSize); this.ground.add(a); break;
+				case 10:a.setCollider('rectangle',0,0,d.tileSize,d.tileSize); this.ground.add(a); break;
 				case 11: case 12: case 13:
 				case 14:console.log('b'); this.tree.add(a); break;
 				case 15: case 16:
