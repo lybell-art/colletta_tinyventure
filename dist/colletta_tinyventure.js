d95f9752ce3748b4a706469d8a827fb44524d902
steliviere <32293736+steliviere@users.noreply.github.com>
2018-06-23 02:14:28 +0900

    Update colletta_tinyventure.js

--- dist/colletta_tinyventure.js
+++ dist/colletta_tinyventure.js
@@ -79,11 +79,11 @@ var game=function(d)
 			{
 				for(var j=0;j<mapHei;j++)
 				{
-					this.makeSpriteSet();
+					this.makeSpriteSet(i,j);
 				}
 			}
 		}
-		WORLD.prototype.makeSpriteSet=function()
+		WORLD.prototype.makeSpriteSet=function(i,j)
 		{
 			var a=d.createSprite(i*d.tileSize,j*d.tileSize,d.tileSize,d.tileSize);
 			var tileNo=d.resourceBox.map[g.currentWorld].platform[j][i];
