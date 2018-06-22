136311b67733bdc35bf55a5d22dccc56874e0c30
steliviere <32293736+steliviere@users.noreply.github.com>
2018-06-23 04:44:35 +0900

    Update colletta_tinyventure.js

--- dist/colletta_tinyventure.js
+++ dist/colletta_tinyventure.js
@@ -50,12 +50,12 @@ var game=function(d)
 		this.run=function()
 		{
 			d.background(255);
-//			d.image(d.resourceBox.image.bg[0],0,0);
+			d.image(d.resourceBox.image.bg[0],0,0);
 //			d.camera.position.x=this.colletta.x;
 //			d.camera.position.y=this.colletta.y;
-//			this.colletta.move(this);
-//			this.colletta.pose(this);
-			d.drawSprites();
+			this.colletta.move(this);
+			this.colletta.pose(this);
+//			d.drawSprites();
 			console.log(d.frameRate());
 		}
 		function PLAYER(g)
