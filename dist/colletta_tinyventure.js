cf97af8d22d830378aac7639f02f59aaba520068
steliviere <32293736+steliviere@users.noreply.github.com>
2018-06-25 01:27:06 +0900

    Update colletta_tinyventure.js

--- dist/colletta_tinyventure.js
+++ dist/colletta_tinyventure.js
@@ -53,9 +53,9 @@ var game=function(d)
 		}
 		this.run=function()
 		{
-//			d.camera.off();
+			d.camera.off();
 			d.image(d.resourceBox.image.bg[0],0,0);
-//			d.camera.on();
+			d.camera.on();
 			this.world.run(this.colletta);
 			this.colletta.physic(this);
 			this.colletta.move(this);
@@ -63,7 +63,6 @@ var game=function(d)
 			d.camera.position.x=d.constrain(this.colletta.sprite.position.x,960,this.world.width-960);
 			d.camera.position.y=d.constrain(this.colletta.sprite.position.y,540,this.world.height-540);
 			d.camera.zoom/=this.colletta.sprite.scale;
-			console.log(d.camera.zoom,this.colletta.sprite.scale);
 			d.drawSprites();
 		}
 		function PLAYER(g)
