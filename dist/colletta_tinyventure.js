6c13c30796ee53ebf39cd4acccffeb6b07af2092
steliviere <32293736+steliviere@users.noreply.github.com>
2018-06-24 20:17:47 +0900

    Update colletta_tinyventure.js

--- dist/colletta_tinyventure.js
+++ dist/colletta_tinyventure.js
@@ -134,9 +134,12 @@ var game=function(d)
 				else return false;
 			});
 			var onRope=this.sprite.overlap(g.world.Vrope);
-			var onGround=this.floorCollider.overlap(g.world.allPlatform);
-			var onWall=this.wallCollider.overlap(g.world.ground);
-			var onCeil=this.ceilCollider.overlap(g.world.ground);
+//			var onGround=this.floorCollider.overlap(g.world.allPlatform);
+//			var onWall=this.wallCollider.overlap(g.world.ground);
+//			var onCeil=this.ceilCollider.overlap(g.world.ground);
+			var onGround=this.sprite.touching.bottom;
+			var onWall=this.sprite.touching.left||this.sprite.touching.right;
+			var onCeil=this.sprite.touching.top;
 			if(onRope)
 			{
 				this.walling=false;
