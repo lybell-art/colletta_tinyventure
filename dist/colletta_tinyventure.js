4519a56936a817a6e884101c2b71a0fb7ab7791d
steliviere <32293736+steliviere@users.noreply.github.com>
2018-06-24 16:26:32 +0900

    Update colletta_tinyventure.js

--- dist/colletta_tinyventure.js
+++ dist/colletta_tinyventure.js
@@ -118,7 +118,7 @@ var game=function(d)
 					var p=a.previousPosition.copy().add(0,a.height/2);
 					var q=b.position.copy().add(0,-b.height/2);
 					var r=p5.Vector.sub(p,q);
-					return [Math.abs(r.y)<0.0001&&Math.abs(r.x)<80,r.heading()<=0];
+					return [Math.abs(r.y)<0.0001,r.heading()<=0];
 				}
 				else return false;
 			});
