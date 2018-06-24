d03f44ac6d47abc9bd33d0d50cecc2db48857aa2
steliviere <32293736+steliviere@users.noreply.github.com>
2018-06-24 19:50:33 +0900

    Update colletta_tinyventure.js

--- dist/colletta_tinyventure.js
+++ dist/colletta_tinyventure.js
@@ -105,7 +105,7 @@ var game=function(d)
 		{
 			var P;
 			var v=this.sprite.velocity.y;
-			if(this.walling&&this.sprite.velocity.x==0) P='wall';
+			if(this.walling&&this.sprite.velocity.x!=0) P='wall';
 			else if(this.jumping) P='jump';
 			else if(this.roping) P='rope';
 			else if(this.sprite.velocity.x!=0) P='walk';
